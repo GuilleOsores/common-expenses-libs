e1c5583b6f37c404dd98e10d2878a6fe9cb01177
pelusa <guille.osores@gmail.com>
2018-12-27 17:50:43 -0300

    first commit

---
+++ libs/building.ts
@@ -0,0 +1,11 @@
+export class Building {
+
+    private id: any;
+    private name: string;
+
+    constructor (id:any, name: string){
+        this.id = id;
+        this.name = name;
+    }
+    
+}
