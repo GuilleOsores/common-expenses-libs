9806b1a2aec1a5279a3a464f724ffa7107bb77e9
pelusa <guille.osores@gmail.com>
2019-01-18 20:37:43 -0300

    user and password

--- libs/user.ts
+++ libs/user.ts
@@ -2,6 +2,8 @@ import { Role } from './role';
 
 export interface User {
     usersId: string;
+    user: string,
+    password: string,
     name: string;
     lastName: string;
     roles: Role[];
